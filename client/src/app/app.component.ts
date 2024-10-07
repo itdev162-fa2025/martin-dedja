7d028ae77a2839863fe1b8430dbe4b16268b1164
Martin Dedja <martindedja11@gmail.com>
2024-10-06 20:45:50 -0500

    Connected to api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  weatherForecast: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5132/weatherforecast').subscribe({
+      next: (response) => this.weatherForecast = response,
+      error: (error) => console.error(error),
+      complete: () => console.log('complete')
+    });
+  }
+
 }
